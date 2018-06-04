664e8c5fd63d31914c21fd59e90f35f63a60c3f1
taylor0417 <taylor57568@163.com>
2018-06-05 00:10:40 +0800

    Add Cookie

--- login.php
+++ login.php
@@ -1,15 +1,20 @@
 <?php
-  var_dump($_POST['check']);
   header('Content-Type: text/html; charset=UTF-8');
   $info = file_get_contents('./users.json');
   $info_arr = json_decode($info,true);
   $username = $_POST['name'];
   $password = $_POST['password'];
+  $flag = false;
   foreach($info_arr as $key=>$val){
     if($username == $val['email'] && $password == $val['password']){
       echo '登陆成功';
-      header('Refresh: 2; url=index.html');
-    }else{
-      echo '输入有误';
+      setcookie('is_login',1,time()+3600*24*7);
+      $flag = true;
     }
+  }
+  if($flag){
+    header('Refresh: 2; url=index.php');
+  }else{
+    echo '登陆失败';
+    header('Refresh: 2; url=login.html');
   }
